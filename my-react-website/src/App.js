9cb0edc9d72207530bdd2f72bde9cb13329132e1
Aliza Hashmat <myaliza.005@gmail.com>
2024-12-22 12:23:45 +0000

    Initial commit

---
+++ my-react-website/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <p>This is the content of the App component.</p>
+    </div>
+  );
+}
+
+export default App;
+
